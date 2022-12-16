c73b004e76839a28bdd7286528638a8d6fce6710
fmtabbara <fmtabbara@hotmail.co.uk>
2022-12-16 16:42:25 +0000

    add additional properties to gateway type

--- nym-wallet/src/context/bonding.tsx
+++ nym-wallet/src/context/bonding.tsx
@@ -8,7 +8,7 @@ import {
 } from '@nymproject/types';
 import React, { createContext, useCallback, useContext, useEffect, useMemo, useState } from 'react';
 import Big from 'big.js';
-import { isGateway, isMixnode, TBondGatewayArgs, TBondMixNodeArgs, TBondMoreArgs } from 'src/types';
+import { EnumNodeType, isGateway, isMixnode, TBondGatewayArgs, TBondMixNodeArgs, TBondMoreArgs } from 'src/types';
 import { Console } from 'src/utils/console';
 import {
   bondGateway as bondGatewayRequest,
@@ -80,7 +80,7 @@ export interface TBondedGateway {
   identityKey: string;
   ip: string;
   bond: DecCoin;
-  location?: string; // TODO not yet available, only available in Network Explorer API
+  location?: string;
   proxy?: string;
   host: string;
   httpApiPort: number;
@@ -91,7 +91,6 @@ export interface TBondedGateway {
     current: number;
     average: number;
   };
-  isUnbonding: boolean;
 }
 
 export type TokenPool = 'locked' | 'balance';
@@ -309,21 +308,24 @@ export const BondingContextProvider = ({ children }: { children?: React.ReactNod
       }
     }
 
-    if (ownership.hasOwnership) {
+    if (ownership.hasOwnership && ownership.nodeType === EnumNodeType.gateway) {
       try {
         const data = await getGatewayBondDetails();
         if (data) {
+          const { gateway, proxy } = data;
           const nodeDescription = await getNodeDescription(data.gateway.host, data.gateway.clients_port);
           const routingScore = await getGatewayReportDetails(data.gateway.identity_key);
           setBondedNode({
             name: nodeDescription?.name,
-            identityKey: data.gateway.identity_key,
-            ip: data.gateway.host,
-            location: data.gateway.location,
+            identityKey: gateway.identity_key,
+            mixPort: gateway.mix_port,
+            httpApiPort: gateway.clients_port,
+            host: gateway.host,
+            ip: gateway.host,
+            location: gateway.location,
             bond: decCoinToDisplay(data.pledge_amount),
-            proxy: data.proxy,
+            proxy: proxy,
             routingScore,
-            isUnbonding: false,
           } as TBondedGateway);
         }
       } catch (e: any) {
