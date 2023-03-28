ba74552b30049d299de6b9c005a7e79a4f109a6c
jackykwe <40329372+jackykwe@users.noreply.github.com>
2023-03-28 03:13:40 +0100

    Allowed changing of delay parameters

---
+++ clients/client-core/src/config/autogenerated.rs
@@ -0,0 +1,8 @@
+// Auto-generated from a script, direct updates may be overwritten during build process
+
+use std::time::Duration;
+
+pub(crate) const THOUSAND_OVER_LAMBDA_L: Duration = Duration::from_millis(200); // LAMBDA_L is in seconds
+pub(crate) const THOUSAND_OVER_LAMBDA_P: Duration = Duration::from_millis(20); // LAMBDA_P is in seconds
+pub(crate) const THOUSAND_OVER_MU: Duration = Duration::from_millis(50); // MU is in seconds
+pub(crate) const THOUSAND_OVER_MU_ACK: Duration = Duration::from_millis(50); // MU_ACK is in seconds
